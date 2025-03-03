892c863497929d780dc6258eed79088cf6398c99
iPlugin <ipluginua@gmail.com>
2025-03-03 17:58:29 +0200

    Start point file: run-project.cpp

---
+++ run_project.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    // Print logo
+
+    if (argc != 2) {
+        cerr << "|>> Usage: " << argv[0] << " <FQDN>" << endl;
+        return 1;
+    }
+
+    // Start program
+
+    return 0;
+}
